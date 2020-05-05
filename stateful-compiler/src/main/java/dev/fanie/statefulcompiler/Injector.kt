ce63f2eee00b9dd9fccfc337b2546766b1b55590
Flavio Veizi <flaviov@spotify.com>
2020-05-05 16:51:27 +0300

    Remove dependency warnings

--- stateful-compiler/src/main/java/dev/fanie/statefulcompiler/Injector.kt
+++ stateful-compiler/src/main/java/dev/fanie/statefulcompiler/Injector.kt
@@ -4,10 +4,10 @@ import java.io.File
 import javax.annotation.processing.ProcessingEnvironment
 
 fun inject(instance: StatefulCompiler, env: ProcessingEnvironment) {
-    val generationDirectory = File(env.options[OPTION_GEN_PACKAGE])
+    val generationDirectory = File(env.options[OPTION_GEN_PACKAGE] ?: "")
     val classGenerator = ClassGenerator(generationDirectory)
     instance.classGenerator = classGenerator
     instance.messager = env.messager
-    val processingUtils = ProcessingUtils(env.elementUtils, env.typeUtils)
+    val processingUtils = ProcessingUtils(env.elementUtils)
     instance.processingUtils = processingUtils
 }
--- stateful-compiler/src/main/java/dev/fanie/statefulcompiler/ProcessingUtils.kt
+++ stateful-compiler/src/main/java/dev/fanie/statefulcompiler/ProcessingUtils.kt
@@ -1,13 +1,14 @@
 package dev.fanie.statefulcompiler
 
 import javax.lang.model.element.Modifier
+import javax.lang.model.element.PackageElement
 import javax.lang.model.element.TypeElement
 import javax.lang.model.util.ElementFilter
 import javax.lang.model.util.Elements
-import javax.lang.model.util.Types
 
-class ProcessingUtils(private val elementUtils: Elements, private val typeUtils: Types) {
-    fun getPackageOf(typeElement: TypeElement) = elementUtils.getPackageOf(typeElement)
+class ProcessingUtils(private val elementUtils: Elements) {
+    fun getPackageOf(typeElement: TypeElement): PackageElement =
+        elementUtils.getPackageOf(typeElement)
 
     fun getClassOf(typeElement: TypeElement) = typeElement.asType().toString()
 
@@ -16,6 +17,6 @@ class ProcessingUtils(private val elementUtils: Elements, private val typeUtils:
             !method.simpleName.startsWith(CLASS_GETTER_PREFIX)
                     && method.modifiers.contains(Modifier.PUBLIC)
                     && (method.simpleName.startsWith(GETTER_PREFIX)
-                        || method.simpleName.startsWith(BOOLEAN_GETTER_PREFIX))
+                    || method.simpleName.startsWith(BOOLEAN_GETTER_PREFIX))
         }
 }
