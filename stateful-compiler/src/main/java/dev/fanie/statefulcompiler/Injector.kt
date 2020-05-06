53dbe0313361ba176ceb334a44e8c8973a8b8f18
Flavio Veizi <flaviov@spotify.com>
2020-05-06 16:43:56 +0300

    Add injector unit tests

--- stateful-compiler/src/main/java/dev/fanie/statefulcompiler/Injector.kt
+++ stateful-compiler/src/main/java/dev/fanie/statefulcompiler/Injector.kt
@@ -4,10 +4,31 @@ import java.io.File
 import javax.annotation.processing.ProcessingEnvironment
 
 fun inject(instance: StatefulCompiler, env: ProcessingEnvironment) {
+    injectClassGenerator(env, instance)
+    injectMessager(instance, env)
+    injectProcessingUtils(env, instance)
+}
+
+private fun injectProcessingUtils(
+    env: ProcessingEnvironment,
+    instance: StatefulCompiler
+) {
+    val processingUtils = ProcessingUtils(env.elementUtils)
+    instance.processingUtils = processingUtils
+}
+
+private fun injectMessager(
+    instance: StatefulCompiler,
+    env: ProcessingEnvironment
+) {
+    instance.messager = env.messager
+}
+
+private fun injectClassGenerator(
+    env: ProcessingEnvironment,
+    instance: StatefulCompiler
+) {
     val generationDirectory = File(env.options[OPTION_GEN_PACKAGE] ?: "")
     val classGenerator = ClassGenerator(generationDirectory)
     instance.classGenerator = classGenerator
-    instance.messager = env.messager
-    val processingUtils = ProcessingUtils(env.elementUtils)
-    instance.processingUtils = processingUtils
 }
---
+++ stateful-compiler/src/test/java/dev/fanie/statefulcompiler/WontDo.kt
@@ -0,0 +1,9 @@
+package dev.fanie.statefulcompiler
+
+/**
+ * Always throws [NotImplementedError] stating that operation will not be implemented.
+ *
+ * @param reason a string explaining why the implementation will not be implemented.
+ */
+fun WONTDO(reason: String = "Not needed for tests"): Nothing =
+    throw NotImplementedError("The operation will not be implemented implemented: $reason")
