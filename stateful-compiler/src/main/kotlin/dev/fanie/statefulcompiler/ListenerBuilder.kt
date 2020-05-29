2c3cfc5456ba3210b04883f7d2e3c7f6a4451f17
Flavio Veizi <flaviov@spotify.com>
2020-05-29 09:12:49 +0300

    Add kdoc to generated listener functions

--- stateful-compiler/src/main/kotlin/dev/fanie/statefulcompiler/ListenerBuilder.kt
+++ stateful-compiler/src/main/kotlin/dev/fanie/statefulcompiler/ListenerBuilder.kt
@@ -46,17 +46,18 @@ class ListenerBuilder(
     private val functions
         get() = buildString {
             statefulGetters.forEachIndexed { index, getter ->
-                val space = if (index > 0) "    " else ""
-
-                val name = getter.name.capitalize()
+                val name = getter.name
                 val type = getter.type
-                append(
-                    """ |${space}${newValue(name, type)}
-                        |    ${bothValues(name, type)}
-                        |    ${newStateful(name)}
-                        |    ${bothStatefuls(name)}
-                    """.trimMargin()
-                )
+                val initialSpace = if (index > 0) "    " else ""
+
+                append(initialSpace)
+                append(newValue(name, type))
+                append("\n\n")
+                append(bothValues(name, type))
+                append("\n\n")
+                append(newStateful(name))
+                append("\n\n")
+                append(bothStatefuls(name))
 
                 if (index < statefulGetters.lastIndex) {
                     append("\n\n")
@@ -64,14 +65,36 @@ class ListenerBuilder(
             }
         }
 
-    private fun newValue(name: String, type: String) = "fun on${name}Updated(new$name: $type) {}"
+    private fun newValue(name: String, type: String) = """/**
+                        |     * Invoked on updates of the [$statefulName.$name] property.
+                        |     * @param new${name.capitalize()} The new $name to be rendered.
+                        |     */
+                        |    fun on${name.capitalize()}Updated(new${name.capitalize()}: $type) {}
+                        """.trimMargin()
 
-    private fun bothValues(name: String, type: String) =
-        "fun on${name}Updated(current$name: $type?, new$name: $type) {}".replace("??", "?")
+    private fun bothValues(name: String, type: String) = """
+                        |    /**
+                        |     * Invoked on updates of the [$statefulName.$name] property.
+                        |     * @param current${name.capitalize()} The currently rendered $name, if any.
+                        |     * @param new${name.capitalize()} The new $name to be rendered.
+                        |     */
+                        |     fun on${name.capitalize()}Updated(current${name.capitalize()}: $type?, new${name.capitalize()}: $type) {}
+                        """.trimMargin().replace("??", "?")
 
-    private fun newStateful(name: String) =
-        "fun on${name}Updated(new$statefulName: $statefulName) {}"
+    private fun newStateful(name: String) = """
+                        |    /**
+                        |     * Invoked on updates of the [$statefulName.$name] property.
+                        |     * @param new$statefulName The new ${statefulName.decapitalize()} to be rendered.
+                        |     */
+                        |     fun on${name.capitalize()}Updated(new$statefulName: $statefulName) {}
+                        """.trimMargin()
 
-    private fun bothStatefuls(name: String) =
-        "fun on${name}Updated(current$statefulName: $statefulName?, new$statefulName: $statefulName) {}"
+    private fun bothStatefuls(name: String) = """
+                        |    /**
+                        |     * Invoked on updates of the [$statefulName.$name] property.
+                        |     * @param current$statefulName The currently rendered ${statefulName.decapitalize()}, if any.
+                        |     * @param new$statefulName The new ${statefulName.decapitalize()} to be rendered.
+                        |     */
+                        |     fun on${name.capitalize()}Updated(current$statefulName: $statefulName?, new$statefulName: $statefulName) {}
+                        """.trimMargin()
 }
